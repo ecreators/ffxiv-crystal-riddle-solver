bd4828983d64c337665e9243e1b636420c709162
ecreators <bf2782@web.de>
2017-06-04 20:03:18 +0200

    workspace optimized

--- src/main/de/easycreators/sequencer/decode/demo/model/Choice.java
+++ src/main/de/easycreators/sequencer/decode/demo/model/Choice.java
@@ -1,8 +1,5 @@
 package de.easycreators.sequencer.decode.demo.model;
 
-import java.util.Arrays;
-import java.util.Random;
-
 /**
  * @author Bjoern Frohberg, mydata GmbH
  */
@@ -11,23 +8,8 @@ public enum Choice {
 	BACKWARD(-1);
 	
 	public final int f;
-	public static final Random RANDOM = new Random(System.currentTimeMillis());
 	
 	Choice(int f) {
 		this.f = f;
-	}
-	
-	public Choice opposite() {
-		return get(-f);
-	}
-	
-	private static Choice get(int f) {
-		return Arrays.stream(Choice.values()).filter(m -> m.f == f).findFirst().orElse(null);
-	}
-	
-	public static Choice random() {
-		return get((int) Math.round(RANDOM.nextGaussian()) == 1
-		           ? 1
-		           : -1);
 	}
 }
